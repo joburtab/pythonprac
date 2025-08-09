bc27b39c6dc252466a6ce129e87f06903caeaf65
abjab12 <abraham.sh124@gmail.com>
2025-08-10 00:33:01 +0300

    interactive quize

---
+++ quize.py
@@ -0,0 +1,27 @@
+print("wecome to quize center ")
+
+quizii=input("do you want take quize ?")
+if quizii != "yes" :
+    quit()
+print("you are interested  lets go: ")
+
+answer=input("ehat is cpu")
+if answer == "centarl procceing unit":
+    print("you are correct")
+else:
+    print("you are not correct ")
+answer=input("ehat is Ram")
+if answer == "centarl procceing unit":
+    print("you are correct")
+else:
+    print("you are not correct ")
+answer=input("ehat is dom")
+if answer == "centarl procceing unit":
+    print("you are correct")
+else:
+    print("you are not correct ")
+answer=input("ehat is rom")
+if answer == "centarl procceing unit":
+    print("you are correct")
+else:
+    print("you are not correct ")
